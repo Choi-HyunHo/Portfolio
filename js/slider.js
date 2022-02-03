cfaa1fe9fba8b6f4b5c587f0f9730cb48303b5bc
com <x7788@naver.com>
2022-02-03 17:48:02 +0900

    section_5 슬라이드로 변경

---
+++ js/slider.js
@@ -0,0 +1,27 @@
+$('.slider').slick({
+  centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 3,
+  arrows: false,
+  autoplay: true,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1,
+      },
+    },
+  ],
+})
